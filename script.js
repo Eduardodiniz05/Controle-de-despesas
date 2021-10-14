8190871710350ebf189c7b6166f43fc6b51e405b
Eduardodiniz05 <eduardo.diniz005@gmail.com>
2021-10-14 17:56:57 -0300

    projeto terminado, inicio do refatoramento de código

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ const form = document.querySelector('#form');
 const inputTransactionName = document.querySelector('#text');
 const inputTransactionAmount = document.querySelector('#amount');
 
-const dummyTransations = [
-    { id: 1, name: 'bolo de brigadeiro', amount: -20 },
-    { id: 2, name: 'salário', amount: 300},
-    { id: 3, name: 'torta de frango', amount: -10 },
-    { id: 4, name: 'violão', amount: 150 },
-]
+
+
+const localStorageTransaction = JSON.parse(localStorage
+    .getItem('transactions'))
+
+let transactions = localStorage
+    .getItem('transactions') !== null ? localStorageTransaction : []
+
+const removeTransaction = ID => {
+    transactions = transactions.filter(transaction => 
+        transaction.id !== ID)
+        updateLocalStorage()
+    init()
+}
 
 const transationIntoDOM = transaction => {
     const operator = transaction.amount < 0 ? '-' : '+';
@@ -32,21 +40,27 @@ const transationIntoDOM = transaction => {
   transactionUl.append(li)
 }
 
-const updatedBalanceValues = () => {
-    const transactionsAmounts = dummyTransations
-        .map(transaction => transaction.amount)
-    const total = transactionsAmounts
-        .reduce((accumulator, transaction ) => accumulator + transaction, 0)
-        .toFixed(2)
-    const income = transactionsAmounts
-        .filter(value => value > 0)
-        .reduce((accumulator, value)=> accumulator + value, 0)
-        .toFixed(2)
-
-        const expense = Math.abs( transactionsAmounts
+const getExpenses = transactionsAmounts => Math.abs( transactionsAmounts
         .filter(value => value < 0)
         .reduce((accumulator, value) => accumulator + value, 0))
         .toFixed(2)
+
+
+const getIncome = transactionsAmounts  => transactionsAmounts.filter(value => value > 0)
+.reduce((accumulator, value)=> accumulator + value, 0)
+.toFixed(2)
+
+
+const getTotal = transactionsAmounts =>  transactionsAmounts
+        .reduce((accumulator, transaction ) => accumulator + transaction, 0)
+        .toFixed(2)
+
+
+const updatedBalanceValues = () => {
+    const transactionsAmounts = transactions.map(transaction => transaction.amount)
+    const total =  getTotal(transactionsAmounts)
+    const income = getIncome(transactionsAmounts)
+    const expense = getExpenses(transactionsAmounts)
         
         
         balanceDisplay.textContent = `R$ ${total}`
@@ -57,35 +71,46 @@ const updatedBalanceValues = () => {
 
 const init = () => {
     transactionUl.innerHTML = ''
-    dummyTransations.forEach(transationIntoDOM)
+    transactions.forEach(transationIntoDOM)
     updatedBalanceValues()
 }
 
 init()
 
-const generatedID = () => Math.round(Math.random() * 1000)
+const updateLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions))
+}
 
-form.addEventListener('submit', event => {
+const addToTransactionArray = (transactionName, transactionAmount) => {
+    transactions.push({ 
+        id: generatedID(), 
+        name: transactionName, 
+        amount: Number(transactionAmount) })
+
+}
+
+const cleanInputs = () => {
+    inputTransactionName.value = ''
+    inputTransactionAmount.value = ''
+}
+
+const handleFormSubmit = event => {
     event.preventDefault()
     const transactionName = inputTransactionName.value.trim();
     const transactionAmount = inputTransactionAmount.value.trim();
+    const isSomeInputEmpty = transactionName === '' || transactionAmount === ''
 
-
-    if(transactionName === '' || transactionAmount === '') {
+    if(isSomeInputEmpty) {
         alert("Por favor, preencha tanto o nome quanto o valor da transação!!!")
      return
     }
 
-    const transaction = { 
-        id: generatedID(), 
-        name: transactionName, 
-        amount: Number(transactionAmount) }
-
-    console.log(transaction)
-
-    dummyTransations.push(transaction)
+    addToTransactionArray(transactionName, transactionAmount)
     init()
+    updateLocalStorage()
+    cleanInputs()
+}
 
-    inputTransactionName.value = ''
-    inputTransactionAmount.value = ''
-})
+const generatedID = () => Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', handleFormSubmit )
