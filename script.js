534a39243e681803bf883009b92b15bb3ee60107
Eduardodiniz05 <eduardo.diniz005@gmail.com>
2021-10-14 10:02:55 -0300

    criada as funcionalidades de armazenamento de dados de saldos positivos, saldos negativos e o balanço das mesmas

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const transactionUl = document.querySelector('#transactions')
 
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+
 const dummyTransations = [
     { id: 1, name: 'bolo de brigadeiro', amount: -20 },
     { id: 2, name: 'salário', amount: 300},
     { id: 3, name: 'torta de frango', amount: -10 },
-    { id: 4, name: 'violão', amount: -150 },
+    { id: 4, name: 'violão', amount: 150 },
 ]
 
 const transationIntoDOM = transaction => {
@@ -22,8 +26,32 @@ const transationIntoDOM = transaction => {
   transactionUl.append(li)
 }
 
+const updatedBalanceValues = () => {
+    const transactionsAmounts = dummyTransations
+        .map(transaction => transaction.amount)
+    const total = transactionsAmounts
+        .reduce((accumulator, transaction ) => accumulator + transaction, 0)
+        .toFixed(2)
+    const income = transactionsAmounts
+        .filter(value => value > 0)
+        .reduce((accumulator, value)=> accumulator + value, 0)
+        .toFixed(2)
+
+        const expense = Math.abs( transactionsAmounts
+        .filter(value => value < 0)
+        .reduce((accumulator, value) => accumulator + value, 0))
+        .toFixed(2)
+        
+        
+        balanceDisplay.textContent = `R$ ${total}`
+        incomeDisplay.textContent =  `R$ ${income}`
+        expenseDisplay.textContent = `R$ ${expense}`
+};
+
+
 const init = () => {
     dummyTransations.forEach(transationIntoDOM)
+    updatedBalanceValues()
 }
 
 init()
